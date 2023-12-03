da17dcb41d2e0b48c302bf725738860c94dee40b
Daniella Botros <daniellabotros@gmail.com>
2023-12-03 14:09:41 +0200

    Update

--- Solution.java
+++ Solution.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.Scanner;
  class Solution {
     public static boolean isPalindrome(int x) {
@@ -26,58 +25,14 @@ import java.util.Scanner;
     }
 
 
-public static int lengthOfLongestSubstring(String s) {
-      
-     if(s.length()==1 || s==""){
-            return 1;
-        }
-
-        int size=0;
-       HashMap<Character, Integer> str=new HashMap<Character,Integer>();
-       int i=0;
-       int start=0;
-       
-
-       while(i<s.length()){
-        if(str.isEmpty()|| !(str.containsKey(s.charAt(i)))){
-            str.put(s.charAt(i),i);
-            i++;
-        }
-        else if(str.containsKey(s.charAt(i))){
-            if(size<str.size()){
-                size=str.size();
-            }
-            str.clear();
-            start++;
-            i=start;
-        }
-
-        if(size<str.size()){
-            size=str.size();
-        }
-
-
-
-        
-       }
-       
-       return size;
-
-    }
 
 
     public static void main(String args[]){
         Scanner myObj = new Scanner(System.in); 
-        //System.out.println("Enter Number");//Palindrom
-        //int x=myObj.nextInt();
-        // System.out.println(isPalindrome(x));
-        System.out.println("Enter String");
-        String s=myObj.nextLine();
-    
-        System.out.println(lengthOfLongestSubstring(s));
-
-
-       
+        System.out.println("Enter Number");//Palindrom
+        int x=myObj.nextInt();
+        System.out.println(isPalindrome(x));
+        myObj.close();
     }
 
 
